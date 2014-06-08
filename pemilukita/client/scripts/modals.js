d709183202036ad8d9319cad8a2cd01aee87a58f
Yoza Wiratama <y_wiratama@yahoo.com>
2014-06-08 22:33:34 +0700

    up

--- pemilukita/client/scripts/modals.js
+++ pemilukita/client/scripts/modals.js
@@ -4,3 +4,9 @@ Template.modal_candidate_info.Name = function () {
         return data.nama;
     }
 };
+Template.modal_candidate_info.Biografi = function () {
+    if (Session.get(SessionRef.Name.ActiveCandidate)) {
+        var data = Session.get(SessionRef.Name.ActiveCandidate);
+        return data.biografi;
+    }
+};
