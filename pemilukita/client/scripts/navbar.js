0562344567aaea3288704b38e102282693b3993a
Yoza Wiratama <y_wiratama@yahoo.com>
2014-06-08 22:25:35 +0700

    up lagi

--- pemilukita/client/scripts/navbar.js
+++ pemilukita/client/scripts/navbar.js
@@ -8,3 +8,11 @@ Template.navbar.events({
         });
     }
 });
+
+Template.navbar.IsLoggedIn = function () {
+    if (Meteor.userId()) return true;
+};
+
+Template.navbar.ActiveUsername = function () {
+    if (Meteor.user()) return Meteor.user().profile.name;
+};
