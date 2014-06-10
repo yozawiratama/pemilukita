4e31ed52b3695612065f394e3dbe009fb317a167
bagofair <dhika.ahmad.aulia@gmail.com>
2014-06-10 21:31:13 +0700

    iseng

--- pemilukita/client/scripts/reason.js
+++ pemilukita/client/scripts/reason.js
@@ -19,3 +19,25 @@ Template.reason.Disabled = function () {
     }).count() > 0) return true;
     else return false;
 };
+
+Template.reason2.events({
+    'click #btnVoteUp': function () {
+        Meteor.call('ReasonVotesInsert', Meteor.userId(), this._id, function (err) {
+            if (err) alert(err.message);
+        });
+    }
+})
+Template.reason2.IsOwn = function () {
+    if (this.CreatedBy == Meteor.userId()) return true;
+    else return false;
+};
+Template.reason2.AmountOfVote = function () {
+    return ReasonVotes.find({CandidateVote_ID : this._id}).count();
+};
+Template.reason2.Disabled = function () {
+    if (ReasonVotes.find({
+        Voter_ID: Meteor.userId(),
+        CandidateVote_ID: this._id
+    }).count() > 0) return true;
+    else return false;
+};
