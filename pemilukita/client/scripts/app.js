8637bc167e8e42a0fc43d4593a1625e614f2df25
Yoza Wiratama <y_wiratama@yahoo.com>
2014-06-10 15:15:07 +0700

    up

--- pemilukita/client/scripts/app.js
+++ pemilukita/client/scripts/app.js
@@ -10,28 +10,44 @@ Template.app.rendered = function () {
     });
 };
 Template.app.events({
-
+    'click #btnPrabowoInfo': function () {
+        console.log('bowo');
+        var data = Session.get(SessionRef.Name.PresidentialCandidate);
+        Session.set(SessionRef.Name.ActiveCandidateLeft, data.caleg[3]);
+    },
+    'click #btnHattaInfo': function () {
+        var data = Session.get(SessionRef.Name.PresidentialCandidate);
+        Session.set(SessionRef.Name.ActiveCandidateLeft, data.caleg[0]);
+    },
+    'click #btnJokowiInfo': function () {
+        var data = Session.get(SessionRef.Name.PresidentialCandidate);
+        Session.set(SessionRef.Name.ActiveCandidateRight, data.caleg[2]);
+    },
+    'click #btnJKInfo': function () {
+        var data = Session.get(SessionRef.Name.PresidentialCandidate);
+        Session.set(SessionRef.Name.ActiveCandidateRight, data.caleg[1]);
+    },
 });
 
-Template.app.LeftHeader = function(){
+Template.app.LeftHeader = function () {
     if (Session.get(SessionRef.Name.ActiveCandidateLeft)) {
         var data = Session.get(SessionRef.Name.ActiveCandidateLeft);
         return data.nama;
     }
 };
-Template.app.LeftContent = function(){
+Template.app.LeftContent = function () {
     if (Session.get(SessionRef.Name.ActiveCandidateLeft)) {
         var data = Session.get(SessionRef.Name.ActiveCandidateLeft);
         return data.biografi;
     }
 };
-Template.app.RightHeader = function(){
+Template.app.RightHeader = function () {
     if (Session.get(SessionRef.Name.ActiveCandidateRight)) {
         var data = Session.get(SessionRef.Name.ActiveCandidateRight);
         return data.nama;
     }
 };
-Template.app.RightContent = function(){
+Template.app.RightContent = function () {
     if (Session.get(SessionRef.Name.ActiveCandidateRight)) {
         var data = Session.get(SessionRef.Name.ActiveCandidateRight);
         return data.biografi;
